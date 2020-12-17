4e639a8cac26ae3b61af438aeb94c5206fd5f9b7
Juliafhnw <71931281+Juliafhnw@users.noreply.github.com>
2020-12-17 18:55:40 +0100

    Controller & Model

--- src/client/appClasses/App_Controller.java
+++ src/client/appClasses/App_Controller.java
@@ -1,50 +1,301 @@
 package client.appClasses;
 
+import java.util.Optional;
 
 import client.ServiceLocator;
 import client.abstractClasses.Controller;
+import client.commonClasses.Translator;
 import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.WindowEvent;
 
-/**
- * Copyright 2015, FHNW, Prof. Dr. Brad Richards. All rights reserved. This code
- * is licensed under the terms of the BSD 3-clause license (see the file
- * license.txt).
- * 
- * @author Brad Richards
- */
 public class App_Controller extends Controller<App_Model, App_View> {
-    ServiceLocator serviceLocator;
+	public App_View view;
+	public App_Model model;
 
-    public App_Controller(App_Model model, App_View view) {
-        super(model, view);
-        
-     // register ourselves to listen for button clicks
-        view.btnClick.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                buttonClick();
-            }
-        });
+	ServiceLocator serviceLocator;
 
-        // register ourselves to handle window-closing event
-        view.getStage().setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent event) {
-                Platform.exit();
-            }
-        });
-        
-        serviceLocator = ServiceLocator.getServiceLocator();        
-        serviceLocator.getLogger().info("Application controller initialized");
-    }
-    
-    public void buttonClick() {
-        model.incrementValue();
-        String newText = Integer.toString(model.getValue());        
+	private final static int MIN_LENGTH = 3;
+	private final static int MAX_LENGTH = 20;
 
-        view.lblNumber.setText(newText);        
-    }
+	private boolean usernameValid = false;
+	private boolean passwordValid = false;
+
+	// Konstruktor
+	public App_Controller(App_Model model, App_View view) {
+		super(model, view);
+
+		// *** SZENEN WECHSEL ***
+		// Wenn in der App_View der Create Button gedrückt wird,
+		// soll die Sczene gewechselt werden zu der CreateView
+		view.getBtnCreate().setOnAction(event -> {
+			view.getStage().setScene(view.getCreateScene());
+		});
+
+		// Wenn in der CreateView der Cancel Button gedrückt wird,
+		// soll die Sczene gewechselt werden
+		view.getCreateView().getBtnCancel().setOnAction(event -> {
+			view.getCreateView().reset();
+			view.getStage().setScene(getMainScene());
+		});
+
+		// Wenn in der CreateView der Save Button gedrückt wird,
+		// soll die Sczene gewechselt werden
+		view.getCreateView().getBtnSave().setOnAction(event -> {
+			view.getCreateView().reset();
+			view.getStage().setScene(getMainScene());
+		});
+
+		// Wenn in View Button Delete geklickt wird
+		view.getBtnDelete().setOnAction(event -> {
+			ToDo selectedItem = view.getTableView().getSelectionModel().getSelectedItem();
+			Alert alertDelete = new Alert(AlertType.CONFIRMATION);
+			Translator t = ServiceLocator.getServiceLocator().getTranslator();
+			alertDelete.setTitle(t.getString("alert.alertDelet.title"));
+			alertDelete.setHeaderText(t.getString("alert.alertDelet.header"));
+
+			Button okButton = (Button) alertDelete.getDialogPane().lookupButton(ButtonType.OK);
+			okButton.setText(t.getString("alert.buttonOk"));
+			Button cancelButton = (Button) alertDelete.getDialogPane().lookupButton(ButtonType.CANCEL);
+			cancelButton.setText(t.getString("alert.buttonCancel"));
+
+			Optional<ButtonType> result = alertDelete.showAndWait();
+			if (result.get() == ButtonType.OK) {
+				view.getTableView().getItems().remove(selectedItem);
+			} else {
+
+			}
+
+		});
+
+		// Alert für MenuItem Shortcut
+		view.getMenuHelpShortcuts().setOnAction(e -> {
+			Alert shortcutinfo = new Alert(AlertType.INFORMATION);
+
+			Translator t = ServiceLocator.getServiceLocator().getTranslator();
+			shortcutinfo.setTitle(t.getString("alert.shortcutinfo.title"));
+			shortcutinfo.setHeaderText(t.getString("alert.shortcutinfo.header"));
+			shortcutinfo.setContentText(t.getString("alert.shortcutinfo.content"));
+			shortcutinfo.showAndWait();
+
+		});
+
+		// Create Button unter Aktion setzen
+		view.getBtnCreate().setOnAction(this::newToDo);
+
+		// CreateView: Save Button unter Aktion setzen
+		view.getCreateView().getBtnSave().setOnAction(this::saveToDo);
+
+		/**
+		 * 3 ChangeListener für Textfelder Username und Password
+		 */
+		view.getTxtUsername().textProperty().addListener((obserable, oldValue, newValue) -> {
+			validateUsername(newValue);
+		});
+
+		view.getTxtPassword().textProperty().addListener((obserable, oldValue, newValue) -> {
+			validatePassword(newValue);
+		});
+
+		/**
+		 * 3 Wenn kein Zeile in der TableView angewählt ist "DEAKTIVIERE" den DELETE
+		 * Button (DISABLED)
+		 */
+		view.getBtnDelete().disableProperty()
+				.bind(Bindings.isEmpty(view.getTableView().getSelectionModel().getSelectedItems()));
+
+		/**
+		 * Buttons beim ersten Aufruf, leeren Feldern oder keiner Änderung deaktivieren
+		 */
+		view.getCreateView().getBtnSave().setDisable(true);
+
+		// register ourselves to handle window-closing event
+		view.getStage().setOnCloseRequest(evt -> {
+			Alert closingAlert = new Alert(AlertType.CONFIRMATION);
+			Translator t = ServiceLocator.getServiceLocator().getTranslator();
+			closingAlert.setTitle(t.getString("alert.closingAlert.title"));
+			closingAlert.setHeaderText(t.getString("alert.closingAlert.header"));
+
+			Button okButton = (Button) closingAlert.getDialogPane().lookupButton(ButtonType.OK);
+			okButton.setText(t.getString("alert.buttonOk"));
+			Button cancelButton = (Button) closingAlert.getDialogPane().lookupButton(ButtonType.CANCEL);
+			cancelButton.setText(t.getString("alert.buttonCancel"));
+
+			closingAlert.showAndWait().filter(r -> r != ButtonType.OK).ifPresent(r -> evt.consume());
+		});
+
+		/*
+		 * Shortcuts
+		 */
+		// set shortcut ctrl'c to create to do
+		view.getBtnCreate().getScene().getAccelerators()
+				.put(new KeyCodeCombination(KeyCode.C, KeyCombination.CONTROL_DOWN), new Runnable() {
+					public void run() {
+						view.getBtnCreate().fire();
+					}
+				});
+
+		// set shortcut ctrl'd to delete to do
+		view.getBtnDelete().getScene().getAccelerators()
+				.put(new KeyCodeCombination(KeyCode.D, KeyCombination.CONTROL_DOWN), new Runnable() {
+					public void run() {
+						view.getBtnDelete().fire();
+					}
+				});
+		// set shortcut ctrl's to save to do
+		view.getCreateView().getBtnSave().getScene().getAccelerators()
+				.put(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN), new Runnable() {
+					public void run() {
+						view.getCreateView().getBtnSave().fire();
+					}
+				});
+		// set shortcut ctrl'c to cancel
+		view.getCreateView().getBtnCancel().getScene().getAccelerators()
+				.put(new KeyCodeCombination(KeyCode.X, KeyCombination.CONTROL_DOWN), new Runnable() {
+					public void run() {
+						view.getCreateView().getBtnCancel().fire();
+					}
+				});
+
+		serviceLocator = ServiceLocator.getServiceLocator();
+		serviceLocator.getLogger().info("Application controller initialized");
+	}
+
+	protected TableView<ToDo> getTableView() {
+		return view.getTableView();
+	}
+
+//3 METHODEN; UM INPUT ZU VALIDIEREN MIT CHANGELISTENER
+
+//Für Username (Emailadresse)
+	private void validateUsername(String newValue) {
+		boolean valid = false; // email not ok
+
+		// Split on '@': must give us two not-empty parts
+		String[] addressParts = newValue.split("@");
+		if (addressParts.length == 2 && !addressParts[0].isEmpty() && !addressParts[1].isEmpty()) {
+			// We want to split the domain on '.', but split does not give us an empty
+			// string, if the split-character is the last character in the string. So we
+			// first ensure that the string does not end with '.'
+			if (addressParts[1].charAt(addressParts[1].length() - 1) != '.') {
+				// Split domain on '.': must give us at least two parts.
+				// Each part must be at least two characters long
+				String[] domainParts = addressParts[1].split("\\."); // Backslash hebt special characters aus
+				if (domainParts.length >= 2) { // erwartet 2 teile für provider
+					valid = true;
+
+					for (String s : domainParts) {
+						if (s.length() < 2)
+							valid = false;
+					}
+				}
+			}
+		
+
+		// Farben neutralisieren
+		view.getTxtUsername().getStyleClass().remove("usernameNotOk");
+		view.getTxtUsername().getStyleClass().remove("usernameok");
+
+		// Farben setzen, rot = invalid, grün = valid
+		if (valid) {
+			view.getTxtUsername().getStyleClass().add("usernameok");
+		} else {
+			view.getTxtUsername().getStyleClass().add("usernameNotOk");
+		}
+
+		// Speichert true/false-Wert für Button aktivieren
+		usernameValid = valid;
+		// Aktiviert Button, wenn alles korrekt
+		enableLoginButton();
+	}
+	}
+	// für Password
+	public void validatePassword(String newValue) {
+
+		boolean valid = false; // password not ok
+		String password = newValue;
+		
+		if (password.length() < MIN_LENGTH) {
+			System.out.println("Password too short. Minimum " + MIN_LENGTH + " characters are required");
+			valid = false;
+		} else if (password.length() < MAX_LENGTH) {
+			System.out.println("Password too long. Maximum " + MAX_LENGTH + " characters are required");
+			valid = false;
+		} else if (!password.matches(".*\\d.*")) {
+			System.out.println("1 digit is required");
+			valid = false;
+		} else if (!password.matches(".*[A-Z].*")) {
+			System.out.println("1 upper case is required");
+			valid = false;
+		} else if (!password.matches(".*[a-z].*")) {
+			System.out.println("1 lower case is required");
+			valid = false;
+		}
+		valid = true;
+	
+
+	// Farben neutralisieren
+	view.getTxtPassword().getStyleClass().remove("passwordNotOk");
+	view.getTxtUsername().getStyleClass().remove("passwordOk");
+
+	if(valid){
+		view.getTxtPassword().getStyleClass().add("passwordOk"); // setzt css auf grün
+	}else{
+		view.getTxtPassword().getStyleClass().add("passwordNotOk");// setzt css auf rot
+	}
+	
+	// Speichert true/false-Wert für Button aktivieren
+	passwordValid=valid;
+
+	// Aktiviert Button, wenn alles korrekt
+	enableLoginButton();
+
+	}
+	
+	
+	private void enableLoginButton() {
+		boolean valid = passwordValid & usernameValid;
+		view.getBtnLogin().setDisable(!valid);
+
+}
+
+	private void newToDo(ActionEvent event) {
+		
+	}
+
+	private void saveToDo(ActionEvent event) {
+		
+	}
+	// ****** SZENEN WECHSEL ******
+
+	// ****** ENDE SZENEN WECHSEL ******
+
+	// ****** GETTER FUER DIE SZENNEN ******
+	// Methode um die Country Szene aus der App_View zu holen
+	private Scene getCreateScene() {
+		return view.getCreateScene();
+
+	}
+	// Methode um die Country Szene aus der App_View zu holen
+		private Scene getMainScene() {
+			return view.getMainScene();
+		}
+	// ****** ENDE GETTER FUER DIE SZENEN ******
+
+	
+
+
 }
--- src/client/appClasses/App_Model.java
+++ src/client/appClasses/App_Model.java
@@ -1,33 +1,34 @@
-package client.appClasses;
+	package client.appClasses;
 
 import client.ServiceLocator;
 import client.abstractClasses.Model;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import server.Priority;
 
-/**
- * Copyright 2015, FHNW, Prof. Dr. Brad Richards. All rights reserved. This code
- * is licensed under the terms of the BSD 3-clause license (see the file
- * license.txt).
- * 
- * @author Brad Richards
- */
 public class App_Model extends Model {
     ServiceLocator serviceLocator;
-    private int value;
     
+	private static ObservableList<ToDo> toDos = FXCollections.observableArrayList();
+
+    
+    //Konstruktor
     public App_Model() {
-        value = 0;
+        
         
         serviceLocator = ServiceLocator.getServiceLocator();        
         serviceLocator.getLogger().info("Application model initialized");
     }
     
-    public int getValue() {
-        return value;
-    }
+    public static void createToDo(int id, String title, String description, Priority priority, String dueDate) {
+		ToDo toDo = new ToDo(id, title, description, priority, dueDate);		
+
+	}
+
+    public ObservableList<ToDo> getToDos() {
+		return toDos;
+	}
+
     
-    public int incrementValue() {
-        value++;
-        serviceLocator.getLogger().info("Application model: value incremented to " + value);
-        return value;
-    }
+   
 }
