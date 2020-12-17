7b2975c4cc6465f6ab1751278dfbc9953f2badc8
Juliafhnw <71931281+Juliafhnw@users.noreply.github.com>
2020-12-17 18:54:37 +0100

    Client GUI für To Do erfassen

---
+++ src/client/appClasses/CreateView.java
@@ -0,0 +1,215 @@
+package client.appClasses;
+
+import java.util.Locale;
+import java.util.logging.Logger;
+
+import com.sun.java.accessibility.util.Translator;
+
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBase;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import server.Priority;
+
+// 0
+public class CreateView extends GridPane {
+
+	private App_Model model;
+	private App_Controller controller;
+	private Stage stage;
+
+	// 1 Controls used for data processing (Data Entry)
+	private Label lblTitle;
+	private Label lblToDo;
+	private Label lblDescription;
+	private Label lblDueDate;
+	private Label lblPriority;
+
+	// 1
+	// Textfeld für To Do, Description, Due Date
+	private TextField txtToDo = new TextField();
+	private TextField txtDescription = new TextField();
+	private TextField txtDueDate = new TextField();
+
+	// ComboBox für Priority
+	protected ComboBox<Priority> cmbPriority = new ComboBox<Priority>();
+
+	// 1 Buttons 
+	protected Button btnSave = new Button("Save");
+	protected Button btnCancel = new Button("Cancel");
+
+	// Konstruktor
+	public CreateView(Stage stage, App_Model model, App_Controller controller) {
+		this.stage = stage;
+		this.model = model;
+		this.add(createDataEntryPane(), 0, 0);
+		this.add(createControlPane(), 0, 1);
+
+		getStylesheets().add(getClass().getResource("app.css").toExternalForm());
+	}
+
+	// 1 Data Entry Pane
+	private Pane createDataEntryPane() {
+
+		GridPane pane = new GridPane();
+		pane.setId("dataEntry");
+		// Declare the individual controls in the GUI
+		lblTitle = new Label("To Do erstellen");
+		lblTitle.setStyle("-fx-font-weight: bold; -fx-font-size: 16;");
+
+		lblToDo = new Label("To Do");
+		lblDescription = new Label("Description");
+		lblDueDate = new Label("Stichtag");
+		lblPriority = new Label("Priorität");
+		// Fill combos (hol mir die Items,alle hinzufügen von den values der Enums)
+		cmbPriority.getItems().addAll(Priority.values());
+
+		// Organize the layout, add in the controls (col, row)
+		pane.add(lblTitle, 0, 0);
+
+		pane.add(lblToDo, 0, 1);
+		pane.add(txtToDo, 1, 1);
+
+		pane.add(lblDescription, 0, 2);
+		pane.add(txtDescription, 1, 2);
+
+		pane.add(lblDueDate, 0, 3);
+		pane.add(txtDueDate, 1, 3);
+
+		pane.add(lblPriority, 0, 4);
+		pane.add(cmbPriority, 1, 4);
+
+		pane.setVgap(5);
+		pane.setHgap(10);
+		pane.setPadding(new Insets(10, 10, 10, 10));
+
+		return pane;
+	}
+
+	// 1 Data Control Pane
+	private Pane createControlPane() {
+		GridPane pane = new GridPane();
+		pane.setId("controlArea");
+		pane.add(btnSave, 0, 0);
+		pane.add(btnCancel, 1, 0);
+		pane.setHgap(10);
+		pane.setPadding(new Insets(10, 10, 10, 35));
+		return pane;
+	}
+
+	/*
+	 * Methode um die TextFelder und Combobox zu leeren
+	 */
+	public void reset() {
+		this.txtToDo.setText("");
+		this.txtDescription.setText("");
+		this.txtDueDate.setText("");
+		this.cmbPriority.getSelectionModel().clearSelection();
+	}
+
+	// Getter (Model, Controller, Stage)
+	public App_Model getModel() {
+		return model;
+	}
+
+	public App_Controller getController() {
+		return controller;
+	}
+	public Stage getStage() {
+		return stage;
+	}
+
+	// Setter (Model, Controller, Stage)
+	public void setController(App_Controller controller) {
+		this.controller = controller;
+	}
+
+	public void setStage(Stage stage) {
+		this.stage = stage;
+	}
+	public void setModel(App_Model model) {
+		this.model = model;
+	}
+
+	// Getter Fields
+	public TextField getTxtToDo() {
+		return txtToDo;
+	}
+
+	public TextField getTxtDueDate() {
+		return txtDueDate;
+	}
+	
+	public TextField getDescription() {
+		return txtDescription;
+	}
+	public ComboBox<Priority> getCmbPriority() {
+		return cmbPriority;
+	}
+	// Setter Fields
+	public void setTxtToDo(TextField txtToDo) {
+		this.txtToDo = txtToDo;
+	}
+
+	public void setTxtDueDate(TextField txtDueDate) {
+		this.txtDueDate = txtDueDate;
+	}
+
+	public void setTxtDescription(TextField txtDescription) {
+		this.txtDescription = txtDescription;
+	}
+	
+	public void setCmbPriority(ComboBox<Priority> cmbPriority) {
+		this.cmbPriority = cmbPriority;
+	}
+	
+
+	
+	//Getter & Setter Controls
+	public Button getBtnSave() {
+		return btnSave;
+	}
+
+
+	public Button getBtnCancel() {
+		return btnCancel;
+	}
+
+	public void setBtnCancel(Button btnCancel) {
+		this.btnCancel = btnCancel;
+	}
+	
+	public void setBtnSave(Button btnSave) {
+		this.btnSave = btnSave;
+	}
+
+	// Getter Labels
+	public Label getLblTitle() {
+		return lblTitle;
+	}
+
+	public Label getLblToDo() {
+		return lblToDo;
+	}
+
+	public Label getLblDueDate() {
+		return lblDueDate;
+	}
+
+	public Label getLblPriority() {
+		return lblPriority;
+	}
+
+}
